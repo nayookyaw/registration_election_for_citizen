8f7d80e5d8a2ed31c4c27837c2707639807c319d
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2020-07-10 22:47:12 +0800

    add webcam snap with camera

---
+++ templates/static/js/election.js
@@ -0,0 +1,18 @@
+function ShowCam() {
+    Webcam.set({
+        width: 320,
+        height: 240,
+        image_format: 'jpeg',
+        jpeg_quality: 100
+    });
+    Webcam.attach('#my_camera');
+}
+window.onload= ShowCam;
+
+function snap() {
+    Webcam.snap( function(data_uri) {
+        // display results in page
+        document.getElementById('results').innerHTML = 
+        '<img id="image" src="'+data_uri+'"/>';
+      } );      
+}
